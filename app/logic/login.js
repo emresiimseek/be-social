df7dc52fc32a98000e5eceff4e5f12dcab8c5fc5
Emre Simsek <e.simsek@emakina.com.tr>
2022-05-31 01:54:19 +0300

    FEAT: js to ts

--- app/logic/login.js
+++
@@ -1,7 +0,0 @@
-import { apiBase } from './api-base';
-
-export const login = (identifier, password) => {
-  const body = { identifier: identifier, password: password };
-  console.log(body);
-  return apiBase.postRequest('auth/local', { identifier: identifier, password: password });
-};
